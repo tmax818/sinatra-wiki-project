16ebd3a773b8c23c579ba0dfde90a78dfb4a0c6f
Tyler Maxwell <tmax818@mac.com>
2018-03-29 13:16:08 -0700

    add final functionality, wikiword links.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,16 @@
 class Page < ActiveRecord::Base
   belongs_to :user
+
+  def wiki_wordify
+    title.split.collect {|s| s.capitalize}.join
+  end
+
+  def self.find_by_slug(slug)
+  self.all.find do |page|
+    if song.slug == slug
+      @song = song
+      end
+    end
+    @song
+  end
 end
